41ff81a7fdab2b2258476835ab25ff216d686eea
Ansh Rathore <anshrathore276@gmail.com>
2026-01-29 22:00:47 +0530

    ansh

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -149,7 +149,7 @@ export default function Home() {
                 transition={{ delay: 0.8, duration: 0.8 }}
               >
                 <a
-                  href="#project"
+                  href="#projects"
                   className="px-6 py-3 rounded-full font-medium text-lg text-white bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63]
                   shadow-lg hover:scale-105 transition-all"
                 >
